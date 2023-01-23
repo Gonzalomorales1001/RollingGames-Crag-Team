254a3ef9bbe25ecf4b4856613fd24b56f8d251a7
Gonzalomorales1001 <gonzalomorales1001@gmail.com>
2023-01-23 17:48:18 -0300

    finished games list functions, added new styles and updated files directory

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ class game{
 
 const initialData=[
     new game(
-        123,
+        0,
         'FlexBox Froggy',
         'a game where you help Froggy and friends by writing CSS code!',
         'CSS',
@@ -20,13 +20,53 @@ const initialData=[
         'https://flexboxfroggy.com/'
         ),
     new game(
-        124,
+        1,
         'JSRobot',
         'Learn JavaScript by playing a platform game: Control a robot to collect coins, avoid obstacles and reach the flag at the end of the level.',
         'JavaScript',
         'https://www.google.com.ar/url?sa=i&url=https%3A%2F%2Fwww.producthunt.com%2Fproducts%2Fjsrobot&psig=AOvVaw0m2zK6CIFiUpEiegFYli8Q&ust=1674569971525000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJCl_7bx3fwCFQAAAAAdAAAAABAD',
         'https://lab.reaal.me/jsrobot/',
-    )
+    ),
+    new game(
+        2,
+        'Python Challenge',
+        'The most entertaining way to explore Python. Every puzzle can be solved by a bit of (python) programming.',
+        'Python',
+        'http://www.pythonchallenge.com/logo.jpg',
+        'www.pythonchallenge.com',
+    ),
+    new game(
+        3,
+        'Robocode',
+        `It's an open-source game for programmers, wherein the core mechanic is programming your units' AI. You control your colony by writing JavaScript.`,
+        'JavaScript',
+        'https://robocode.sourceforge.io/gfx/robocode_logo_tanks.png',
+        'https://robocode.sourceforge.io/',
+    ),
+    new game(
+        4,
+        'Screeps',
+        'World is an open source MMO RTS sandbox game for programming enthusiasts, wherein the core mechanic is programming your units AI.',
+        'JavaScript',
+        'https://raw.githubusercontent.com/screeps/screeps/HEAD/logo.png',
+        'https://screeps.com/',
+    ),
+    new game(
+        5,
+        'SQL: Murder Mistery',
+        `There's been a Murder in SQL City! The SQL Murder Mystery is designed to be both a self-directed lesson to learn SQL concepts and commands and a fun game for experienced SQL users to solve an intriguing crime.`,
+        'SQL',
+        'https://www.freecodecamp.org/news/content/images/2020/08/The_SQL_Murder_Mystery.jpg',
+        'https://mystery.knightlab.com/',
+    ),
+    new game(
+        6,
+        'CSS Dinner!',
+        `No worries, you've got this! You're about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.`,
+        'CSS',
+        'https://www.google.com.ar/url?sa=i&url=https%3A%2F%2Fgithub.com%2Fflukeout%2Fcss-diner%2Fissues%2F93&psig=AOvVaw0x5R73Gucfizqq7MtQ9cAt&ust=1674589099893000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKiY3dm43vwCFQAAAAAdAAAAABAI',
+        'https://flukeout.github.io/',
+    ),
 ]
 
 let games=JSON.parse(localStorage.getItem('games'))||initialData
--- js/admin.js
+++ js/admin.js
@@ -1,13 +1,116 @@
-class game{
-  constructor(id,title,description,category,img,url,favorite=false){
-      this.id=id
-      this.title=title
-      this.description=description
-      this.category=category
-      this.img=img
-      this.url=url
-      this.favorite=favorite
-  }
+const editGameModal = new bootstrap.Modal(document.getElementById('editGameModal'))
+
+let tableBody=document.querySelector('#game-table__body')
+
+
+const loadTable=()=>{
+  tableBody.innerHTML=''
+
+  games.forEach(game=>{
+    let tr=document.createElement('tr')
+
+    let content=`
+    <th scope="row">${game.title}</th>
+    <td class="text-justify">${game.description}</td>
+    <td>${game.category}</td>
+    <td class="text-center fs-3"><i class="bi bi-pencil-square" onclick="showEditGameModal(${game.id})"></i></td>
+    <td class="text-center fs-3"><i class="text-danger mx-1 bi bi-trash-fill" onclick="deleteGame(${game.id})"></i></td>
+    `
+    tr.innerHTML=content
+    tableBody.appendChild(tr)
+  })
 }
 
-const myModal = new bootstrap.Modal(document.getElementById('editGameModal'))
+const saveGamesInLS=()=>localStorage.setItem('games',JSON.stringify(games))
+
+const deleteGame=(id)=>{
+  let findGame=games.find(game=>game.id==id)
+  let indexgame=games.findIndex(game=>game.id==id)
+
+  let confirmDelete=confirm(`Are you sure to want to delete ${findGame.title} from Dewbie?`)
+
+  confirmDelete?games.splice(indexgame,1):
+  saveGamesInLS()
+  loadTable()
+}
+
+const addNewGame=(event)=>{
+  event.preventDefault()
+  console.log(`funcion enabled`)
+  let nameInput=document.querySelector('#game-name')
+  let urlInput=document.querySelector('#game-url')
+  let descInput=document.querySelector('#game-description')
+  let categorySelect=document.querySelector('#game-category')
+  let urlImageInput=document.querySelector('#game-imageURL')
+
+  let newGame=new game(
+    new Date().getTime(),
+    nameInput.value,
+    descInput.value,
+    categorySelect.value,
+    urlImageInput.value,
+    urlInput.value
+    )
+
+  games.push(newGame)
+  saveGamesInLS()
+  loadTable()
+
+  nameInput.value=''
+  descInput.value=''
+  categorySelect.value=''
+  urlImageInput.value=''
+  urlInput.value=''
+}
+
+let gameData=null
+
+const showEditGameModal=(id)=>{
+  editGameModal.show()
+  gameData=games.find(game=>game.id==id)
+
+  let nameInput=document.querySelector('#game-name__edit-modal')
+  let urlInput=document.querySelector('#game-url__edit-modal')
+  let descInput=document.querySelector('#game-description__edit-modal')
+  let categorySelect=document.querySelector('#game-category__edit-modal')
+  let urlImageInput=document.querySelector('#game-imageURL__edit-modal')
+
+  nameInput.value=gameData.title
+  descInput.value=gameData.description
+  categorySelect.value=gameData.category
+  urlImageInput.value=gameData.img
+  urlInput.value=gameData.url
+}
+
+const editGame=(event)=>{
+  let index=games.findIndex(game=>game.id==gameData.id)
+  event.preventDefault()
+
+  let nameInput=document.querySelector('#game-name__edit-modal')
+  let urlInput=document.querySelector('#game-url__edit-modal')
+  let descInput=document.querySelector('#game-description__edit-modal')
+  let categorySelect=document.querySelector('#game-category__edit-modal')
+  let urlImageInput=document.querySelector('#game-imageURL__edit-modal')
+
+  games[index].title=nameInput.value
+  games[index].description=descInput.value
+  games[index].category=categorySelect.value
+  games[index].img=urlImageInput.value
+  games[index].url=urlInput.value
+
+  saveGamesInLS()
+  loadTable()
+  editGameModal.hide()
+}
+
+loadTable()
+
+//references
+
+// {/* <tr>
+// <th scope="row">Froggy</th>
+// <td class="text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel enim aspernatur est rem ullam, veniam eligendi modi voluptatibus iure possimus quae error, quis magnam hic quaerat exercitationem nesciunt placeat illum, qui facere et obcaecati officia porro accusamus! Ad ea natus numquam enim dignissimos cum veritatis dolore veniam, atque placeat eos odit magni nesciunt maxime nihil laborum. Inventore quibusdam magni quis.</td>
+// <td>CSS, Javascript</td>
+// <td class="text-center fs-3"><i class="bi bi-pencil-square" data-bs-toggle="modal" data-bs-target="#editGameModal"></i></td>
+// <td class="text-center fs-3"><i class="text-danger mx-1 bi bi-trash-fill"></i></td>
+// </tr> */}
